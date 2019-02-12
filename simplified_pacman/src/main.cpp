29d124dc5d46d1df35f6ee911c5d7c1c79060c72
Sparkles <nathanquan.us@gmail.com>
2019-02-12 11:12:26 -0800

    learning l2

--- simplified_pacman/src/main.cpp
+++ simplified_pacman/src/main.cpp
@@ -66,7 +66,7 @@ bool loadMedia()
     gHelloWorld = SDL_LoadBMP( "hello_world.bmp" );
     if( gHelloWorld == NULL )
     {
-        printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
+        printf( "Unable to load image %s! SDL Error: %s\n", "hello_world.bmp", SDL_GetError() );
         success = false;
     }
     
@@ -110,7 +110,7 @@ int main( int argc, char* args[] )
             SDL_UpdateWindowSurface( gWindow );
             
             //Wait two seconds
-            SDL_Delay( 2000 );
+            SDL_Delay( 5000 );
         }
     }
     
